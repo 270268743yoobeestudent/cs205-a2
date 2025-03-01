540b65afad6d7fd2600a955460dc2d85401f5d71
Billiejean Wills <144515856+beanie108@users.noreply.github.com>
2025-03-01 18:58:38 +1300

    Continue developing front end

---
+++ frontend/src/routes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Login from "./pages/Login"; // Import Login page
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
